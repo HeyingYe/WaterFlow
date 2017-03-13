be93a97b7b92e51e06670e5a22b38d10ce99aec4
yhy <920430533@qq.com>
2017-03-13 13:59:25 +0800

    完善加载更多功能

--- js/global.js
+++ js/global.js
@@ -13,19 +13,31 @@ globalApp.directive('repeatFinish', function () {
     return {
     	restrict: 'A',
         link: function ($scope, element, attrs) {
-            if ($scope.$last === true) {            	
+            if ($scope.$last === true) {    
+                window.onresize = function(){
+                    $scope.repeatFinish("img",5);
+                }
                 $scope.repeatFinish("img",5);
             }
         }
     }
 })
-globalApp.directive("more",function(){
-    return {
-        restrict:"E",
-        replace:false,
-        templateUrl:"html/Imglist.html?_"+Math.random(),
-        controller:function($scope,$http){
+// globalApp.directive("more",function(){
+//     return {
+//         restrict:"E",
+//         templateUrl:"html/Imglist.html?_"+Math.random(),
+//         replace:true,
+//         controller:function($scope,$http){
 
+//         }
+//     }
+// })
+globalApp.service("resur",function(){
+    this.resur = function(num){
+        if(num <= 1){
+            return 1;
+        }else{
+            return num * arguments.callee(num - 1);
         }
     }
 })
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var indexApp = angular.module("indexApp",["FlowGrid","globalApp"]);
+var indexApp = angular.module("indexApp",["ui.router","FlowGrid","globalApp"]);
 //配置路由映射表
 // indexApp.config(function($stateProvider,$urlRouterProvider){
 //     $stateProvider.state('more',{
@@ -6,19 +6,29 @@ var indexApp = angular.module("indexApp",["FlowGrid","globalApp"]);
 //         templateUrl:'html/more.html?' + Math.random()
 //     })
 // })
-
 indexApp.controller("Imglist",["$scope","$http","column","waterFall",function($scope,$http,column,waterFall){
+	var arr = [];
 	$http.get("imglist.json").success(function(res){
-		console.log(res)
-		$scope.arrImg = res;
+		for(var i=0;i<res.length;i++){
+			arr.push(res[i])
+		}
+		$scope.arrImg = arr;
 	})
 	$scope.more = function(){
-		$(".listImg").append("<pic></pic>");
-
+		$http.get("imglist.json").success(function(res){
+			for(var i=0;i<res.length;i++){
+				arr.push(res[i])
+			}
+			console.log(arr);
+			$scope.arrImg = [];
+			$scope.arrImg = arr;
+			// $scope.$apply();
+			// console.log($scope.arrImg);
+		})
 	}
 	$scope.repeatFinish = function(item,cols){
 		setTimeout(function(){
 			waterFall.grid(item,cols);
-		})
+		},200)
 	}
 }])
